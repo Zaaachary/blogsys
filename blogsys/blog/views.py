55fe5173cb0fbbea4e794186f6d28915edd07875
Zachary Lee <li.zaaachary@gmail.com>
2019-05-31 23:08:50 +0800

    修改view.py 从function view 到 class-base view  用户逻辑部分暂告一段落

--- blogsys/blog/views.py
+++ blogsys/blog/views.py
@@ -1,11 +1,98 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from django.views.generic import DetailView, ListView
 
 from .models import Post, Category, Tag
 from config.models import SideBar, Link
 
 
+class CommonViewMixin:
+    def get_context_data(self, **kwargs):
+        """重载get_context_data方法 更新context"""
+        context = super().get_context_data(**kwargs)
+        context.update({
+            # 'sidebars': SideBar.get_all(),
+            'sidebars': self.get_sidebars(),
+        })
+        context.update(self.get_navs())
+        return context
+
+    def get_sidebars(self):
+        return SideBar.objects.filter(status=SideBar.STATUS_SHOW)
+
+    def get_navs(self):
+        categories = Category.objects.filter(status=Category.STATUS_NORMAL)
+        nav_categories = []
+        normal_categories = []
+        for cate in categories:
+            if cate.is_nav:
+                nav_categories.append(cate)
+            else:
+                normal_categories.append(cate)
+
+        return {
+            'navs': nav_categories,
+            'categories': normal_categories,
+        }
+
+
+class IndexView(CommonViewMixin, ListView):
+    queryset = Post.latest_posts()
+    paginate_by = 3
+    context_object_name = 'post_list'
+    template_name = 'blog/list.html'
+
+
+class CategoryView(IndexView):
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        category_id = self.kwargs.get('category_id')
+        category = get_object_or_404(Category, pk=category_id)
+        context.update({
+            'category': category,
+        })
+        return context
+
+    def get_queryset(self):
+        """ 重写queryset，根据分类过滤 """
+        queryset = super().get_queryset()
+        category_id = self.kwargs.get('category_id')
+        return queryset.filter(category_id=category_id)
+
+
+class TagView(IndexView):
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        tag_id = self.kwargs.get('tag_id')
+        tag = get_object_or_404(Tag, pk=tag_id)
+        context.update({
+            'tag': tag,
+        })
+        return context
+
+    def get_queryset(self):
+        """ 重写queryset, 根据标签过滤 """
+        queryset = super().get_queryset()
+        tag_id = self.kwargs.get('tag_id')  # 从url中地参数找tag_id
+        return queryset.filter(tag__id=tag_id)
+
+
+# class PostDetailView(ListView):
+#     """测试ListView"""
+#     queryset = Post.latest_posts()
+#     paginate_by = 1
+#     context_object_name = 'post_list'   # 如不设置此项，要在模板中使用object_list变量
+#     template_name = 'blog/listviewtest.html'
+
+class PostDetailView(CommonViewMixin, DetailView):
+    queryset = Post.latest_posts()
+    template_name = 'blog/detail.html'
+    context_object_name = 'post'
+    pk_url_kwarg = 'post_id'
+
+
 def post_list(request, category_id=None, tag_id=None):
+    """ function view of post_list"""
     tag = None
     category = None
 
@@ -28,6 +115,7 @@ def post_list(request, category_id=None, tag_id=None):
 
 
 def post_detail(request, post_id):
+    """ function view of post_detail"""
     try:
         post = Post.objects.get(id=post_id)
     except Post.DoesNotExist:
--- blogsys/blogsys/base_admin.py
+++ blogsys/blogsys/base_admin.py
@@ -15,4 +15,3 @@ class BaseOwnerAdmin(admin.ModelAdmin):
     def save_model(self, request, obj, form, change):
         obj.owner = request.user
         return super(BaseOwnerAdmin, self).save_model(request, obj, form, change)
-
