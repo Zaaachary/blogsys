5dc2c803f72dcf86af5f51f21e09614e5b37bb92
Zachary Lee <li.zaaachary@gmail.com>
2019-08-08 00:13:16 +0800

    增加友情链接页面 ( /links/

--- blogsys/config/views.py
+++ blogsys/config/views.py
@@ -1,6 +1,14 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic import ListView
+
+from blog.views import CommonViewMixin
+from .models import Link
+
+class LinkListView(CommonViewMixin, ListView):
+    queryset = Link.objects.filter(status=Link.STATUS_NORMAL)
+    template_name = 'config/links.html'
+    context_object_name = 'link_list'
 
 
-def links(request):
-    return HttpResponse('links')
+# def links(request):
+# """return links"""
+#     return HttpResponse('links')
